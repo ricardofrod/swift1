a66c5ce9d8418d048989cb61aa65b888a43a1a4d
Ricardo Rodrigues <ricardorodrigues@MacBook-Pro-de-Ricardo.local>
2017-02-11 16:00:12 +0000

    first commit

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    //MARK: Properties
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   //MARK: UITextFieldDelegate
+   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        nameTextField.text = textField.text
+    }
+    
+ //MARK: Actions
 
+    @IBAction func setDefaultLabelText(_ sender: UIButton){
+        nameTextField.text="Escreva aqui..."
+    }
 
+    
 }
 
